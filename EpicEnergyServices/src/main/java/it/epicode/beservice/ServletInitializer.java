8cf23ff67f5ac18d0fae2f5e61f4c1dcf5beacc0
Lorenzoperrells <lorenzoperrells@gmail.com>
2021-11-14 12:08:00 +0100

    Completed

--- EpicEnergyServices/src/main/java/it/epicode/beservice/ServletInitializer.java
+++ EpicEnergyServices/src/main/java/it/epicode/beservice/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(EpicEnergyServicesSecurityApplication.class);
+		return application.sources(EpicEnergyServicesApplication.class);
 	}
 
 }
